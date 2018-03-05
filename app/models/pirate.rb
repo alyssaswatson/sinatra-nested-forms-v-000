97e7cc8431797e66c9242f400a60ba3c9f0fe22f
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-03-05 22:18:58 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(details)
+    @name = details[:name]
+    @weight = details[:weight]
+    @height = deatails[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
 end
